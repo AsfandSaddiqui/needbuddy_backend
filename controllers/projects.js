3371fdaa728febdbfba12df48b89caf17b77c34a
asfandsaddiqui <asfandsaddiqui@gmail.com>
2021-12-10 01:07:30 +0500

    update project route added

--- controllers/projects.js
+++ controllers/projects.js
@@ -211,4 +211,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+// update  project status 
+router.put("/", async (req, res) => {
+ try {
+    const project = await Project.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    if (!project) return res.status(404).send("No Project Exist with this ID!");
+    //sendig user back
+    res.status(200).send("Project Details Updated Successfully!");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 module.exports = router;
